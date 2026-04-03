1bfd0df901b01c86948b6c95dad58c787cc4c18e
anton_cooper <antncooper@gmail.com>
2026-04-03 12:49:18 -0700

    task three

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+
+nameInput.addEventListener('input', handleInput);
+
+function handleInput(event) {
+  const userInput = event.target.value;
+  const normalizedInput = userInput.trim();
+  if (normalizedInput === '') {
+    nameOutput.textContent = 'Anonymous';
+  } else {
+    nameOutput.textContent = normalizedInput;
+  }
+}
